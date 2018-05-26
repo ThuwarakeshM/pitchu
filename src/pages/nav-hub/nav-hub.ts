96d4eb8e3ac6e97322463c18b554c9eb54b1ce3f
Thuwarakesh <thuwarakesh@gmail.com>
2018-05-26 09:53:53 +0530

    added cards to nav-hub page

--- src/pages/nav-hub/nav-hub.ts
+++ src/pages/nav-hub/nav-hub.ts
@@ -9,7 +9,7 @@ import { AuthProvider } from '../../providers/auth/auth';
  * Ionic pages and navigation.
  */
 
-@IonicPage({name: 'NavHubPage'})
+@IonicPage({ name: 'NavHubPage' })
 @Component({
   selector: 'page-nav-hub',
   templateUrl: 'nav-hub.html',
@@ -18,8 +18,25 @@ export class NavHubPage {
 
   title: string;
 
+  items: Array<{ title: string, img: string, link: string }>;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public auth: AuthProvider) {
     this.title = navParams.get('origin');
+
+    this.items = [];
+
+    if (this.title === 'Market') {
+      this.items.push({ title: 'MyProducts', img: 'assets/imgs/logo.png', link:'#' })
+      this.items.push({ title: 'Favorites', img: 'assets/imgs/logo.png', link:'#' })
+      this.items.push({ title: 'Connections', img: 'assets/imgs/logo.png', link:'#' })
+    } else if (this.title === 'Connections') {
+      this.items.push({ title: 'MyConnections', img: 'assets/imgs/logo.png', link:'#' })
+      this.items.push({ title: 'Requests Sent', img: 'assets/imgs/logo.png', link:'#' })
+      this.items.push({ title: 'Requests Received', img: 'assets/imgs/logo.png', link:'#' })
+    } else if (this.title === 'Orders') {
+      this.items.push({ title: 'Drafted Orders', img: 'assets/imgs/logo.png', link:'#' })
+      this.items.push({ title: 'Actual Orders', img: 'assets/imgs/logo.png', link:'#' })      
+    }
   }
 
   ionViewDidLoad() {
