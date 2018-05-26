21b68aea1853def9df5d277b7e42f1e5aa2f6918
Thuwarakesh <thuwarakesh@gmail.com>
2018-05-26 16:50:46 +0530

    User list page populated with dummy list

--- src/pages/user-list/user-list.ts
+++ src/pages/user-list/user-list.ts
@@ -16,10 +16,44 @@ import { AuthProvider } from '../../providers/auth/auth';
 })
 export class UserListPage {
 
+  items: Array<{
+    id: string
+    name: string,
+    img: string,
+    desc: string
+  }>;
+
   title:  string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public auth: AuthProvider) {
     this.title = navParams.get('origin');
+
+    this.items = [
+      {
+        id:'1',
+        name:'User1',
+        desc:'User1 Desc',
+        img:'assets/imgs/logo.png'
+      },
+      {
+        id:'2',
+        name:'User2',
+        desc:'User2 Desc',
+        img:'assets/imgs/logo.png'
+      },
+      {
+        id:'3',
+        name:'User3',
+        desc:'User3 Desc',
+        img:'assets/imgs/logo.png'
+      },
+      {
+        id:'4',
+        name:'User4',
+        desc:'User4 Desc',
+        img:'assets/imgs/logo.png'
+      }
+    ]
   }
 
   ionViewDidLoad() {
