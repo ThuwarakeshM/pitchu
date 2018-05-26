9bb470c993c896377a54ec289aa9e9ce62e303b2
Thuwarakesh <thuwarakesh@gmail.com>
2018-05-26 18:19:48 +0530

    Products dropdown feature added

--- src/pages/product-list/product-list.ts
+++ src/pages/product-list/product-list.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AuthProvider } from '../../providers/auth/auth';
+import { item } from '../../item';
 
 /**
  * Generated class for the ProductListPage page.
@@ -18,11 +19,10 @@ export class ProductListPage {
 
   title: string;
 
-  items: Array<{
-    id: string
-    name: string,
-    img: string,
-    desc: string
+  item_set: Array<{
+    section_title: string,
+    folded: boolean;
+    items: Array<item>
   }>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public auth: AuthProvider) {
@@ -30,36 +30,64 @@ export class ProductListPage {
 
     // The below is just a sample for dev purposes
     // This should be removed once the services are risen
-    this.items = [
+    this.item_set = [
       {
-        id:'1',
-        name:'Product1',
-        desc:'Product1 Desc',
-        img:'assets/imgs/logo.png'
+        section_title: 'Aperal',
+        folded:true,
+        items: [
+          {
+            id:'1',
+            name:'Order1',
+            desc: 'Order1 Desc',
+            img: 'assets/imgs/logo.png'
+          },
+          {
+            id:'2',
+            name:'Order2',
+            desc: 'Order2 Desc',
+            img: 'assets/imgs/logo.png'
+          },
+          {
+            id:'3',
+            name:'Order3',
+            desc: 'Order3 Desc',
+            img: 'assets/imgs/logo.png'
+          }
+        ]
       },
       {
-        id:'2',
-        name:'Product2',
-        desc:'Product2 Desc',
-        img:'assets/imgs/logo.png'
-      },
-      {
-        id:'3',
-        name:'Product3',
-        desc:'Product3 Desc',
-        img:'assets/imgs/logo.png'
-      },
-      {
-        id:'4',
-        name:'Product4',
-        desc:'Product4 Desc',
-        img:'assets/imgs/logo.png'
+        section_title: 'Cosmetics',
+        folded: true,
+        items: [
+          {
+            id:'4',
+            name:'Order4',
+            desc: 'Order4 Desc',
+            img: 'assets/imgs/logo.png'
+          },
+          {
+            id:'5',
+            name:'Order5',
+            desc: 'Order5 Desc',
+            img: 'assets/imgs/logo.png'
+          },
+          {
+            id:'6',
+            name:'Order6',
+            desc: 'Order6 Desc',
+            img: 'assets/imgs/logo.png'
+          }
+        ]
       }
-    ]
+    ];
   }
 
   ionViewDidLoad() {
     return this.auth.isLoggedIn();
+  }
+
+  foldToggler(sec) {
+    sec.folded = sec.folded ? false : true;
   }
 
 }
